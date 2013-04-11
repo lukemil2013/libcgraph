d0f7f9a6e0b3d65b107dd241fef9f50a21886c91
anthony <anthony@localhost.localdomain>
2013-04-11 14:31:49 -0500

    Updated doc/README and some functions in graph.{c,h}

--- include/cgraph/graph.h
+++ include/cgraph/graph.h
@@ -11,55 +11,132 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
+    
+#ifndef FALSE
+#define FALSE 0
+#define TRUE 1
+#endif
 
     // We'll store data in the vertex structure.
     struct vertex{
+        // data can be of any type.
         void * data;
-        bool visit = false;
+        // This should be one if visited, 0 otherwise.
+        int visit;
+        // To keep track of visitation.
         int previsit;
         int postvisit;
     };
     
-    void visit(vertex & v, bool b);
+    // This may not be needed.
     int Compare(vertex & l, vertex & r);
     
     // Relate two vertices together.
     struct edge{
+        // If edge is one way directional, u should be the starting node.
         vertex * u;
+        // Should be the ending node.
         vertex * v;
+        // Indicate the direction of the edge.
+        // Undirected edges should be treated as two way directional.
         enum type { ONE_WAY, TWO_WAY } direction;
+        // The distance between the two nodes.
         double dist;
     };
     
+    // Check if an edge exists between vertices u and v.
+    // If it exists, return 1 or
     int edgeExists(graph & g, vertex* u, vertex * v);
+    // the distance between them.
     double edgeExist(graph & g, vertex * u, vertex * v);
     
     // Join vertices and edges as lists in a graph.
     struct graph{
+        // This should be an array allocated using malloc and added to
+        // using realloc. Applies to V and E.
         vertex * V;
         edge * E;
-        int * magE;
-        int * magV;
+        // The number of edges in the graph, i.e. size of array.
+        int magE;
+        // The number of vertices in the graph.
+        int magV;
+        // Used for a vertices previsit and postvisit numbers.
         int count;
     };
     
-    // Should return an adjacency matrix of integers.
+    /*
+     * Every graph should be initialized before it is used. Allocates space
+     * for one structure each for the vertex and edge arrays.
+     * Input: A graph reference.
+     */
+    void initGraph(graph & g);
+    
+    /* 
+     * Should return an adjacency matrix of integers. The row and column indices
+     * represent the indices for the vertices in the graph's vertex array. An
+     * edge is represented by a one.
+     * Input: A graph reference.
+     * Output: A two-dimensional integer array of size g.magV * g.magV
+     */
     int * createAdjMatrix(graph & g);
-    // Create an adjacency matrix of doubles.
+    
+    /*
+     * Returns an adjacency matrix of doubles. Same as the integer matrix, but
+     * the non-zero double values represent the distance between the nodes.
+     * Input: A graph reference.
+     * Output: A two-dimensional double array of size g.magV * g.magV
+     */
     double * createAdjMatrix(graph & g);
-    // Create an adjacency list.
+    
+    /*
+     * Returns an adjacency matrix representation of the graph.
+     * Input: A graph reference.
+     * Output: A linked list adjacency matrix.
+     * @TODO: Look into using an array of Lists instead of a List of Lists.
+     * @TODO: It might be easier to work with. Can allocate and expand array
+     * @TODO: using malloc and realloc functions.
+     */
     List * createAdjList(graph & g);
-    // Add a vertex to a graph.
+    
+    /*
+     * Uses realloc to expand the vertex array of the graph and add a new 
+     * vertex to the end.
+     * Input: A graph and vertex reference.
+     * Output: 1 indicates success and -1 indicates an error in the reallocation
+     *          process.
+     */
     int AddVertex(graph & g, vertex & v);
-    // Add an edge to a graph. Must form the edge first.
+    /*
+     * Uses realloc to expand the edge array of the graph and add a new edge to 
+     * to the end.
+     * Input: A graph and edge reference.
+     * Output: 1 indicates success and -1 indicates an error in the reallocation
+     *          process.
+     */
     int AddEdge(graph & g, edge & e);
-    // Explore a graph.
+  
+    /*
+     * Explore a graph vertex by vertex. Used by DepthFirstSearch.
+     * Input: A graph and vertex reference.
+     */
     void explore(graph & g, vertex & v);
+    
+    /*
+     * Explore a graph using a vertex's neighbors first and update it's distance
+     * from a starting vertex.
+     * Input: A graph and vertex reference.
+     */
     void BreadthFirstSearch(graph & g, vertex & origin);
     void DepthFirstSearch(graph & g, vertex & origin);
     void Dijkstra(graph & g, vertex & origin);
     void BellmanFord(graph & g, vertex & origin);
-    // Partition the graph.
+    
+    /*
+     * Partition the graph.
+     * @TODO: Need to think about this one. We could use it for selecting subgraphs
+     * @TODO: or change it to return a list of the vertices connecting the
+     * @TODO: the subgraphs in the graph. Not sure what we are looking for.
+     */
     graph * Partition(graph & g);
     
 
--- src/graph.c
+++ src/graph.c
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <types.h>
 
+void initGraph(graph & g){
+    g.E = NULL;
+    g.V = NULL;    
+    g.count = 0;
+    g.magE = 0
+    g.magV = 0;
+}
+
 List * findEdges(graph & g, vertex * v){
     List * edgeList;
     initList(edgeList);
@@ -89,23 +97,133 @@ List * createAdjList(graph & g){
 
 // Add a vertex to a graph.
 int AddVertex(graph & g, vertex & v){
-    g.magV++;
-    g.V = realloc(g.V, g.magV);
-    g.V[g.magV-1] = v;
+    if(g.V == NULL){
+        g.V = malloc(sizeof(vertex));
+        
+        if(g.V == NULL){
+            return -1;
+        }
+        
+        g.V = v;
+        g.magV++;
+    } else {
+        g.magV++;
+        vertex * oldV = malloc(sizeof(vertex)*g.magV-1);
+    
+        for(int i = 0; i < g.magV-1; i++){
+                oldV[i] = g.V[i];
+        }
+    
+        g.V = realloc(g.V, g.magV);
+        if(g.V = NULL){
+                g.magV--;
+                g.V = oldV;
+                return -1;
+        }
+        g.V[g.magV-1] = v;
+        free(oldV);
+    }
+    
+    return 1;
 }
 
 // Add an edge to a graph. Must form the edge first.
 int AddEdge(graph & g, edge & e){
-    g.magE++;
-    g.E = realloc(g.E, g.magE);
-    g.E[g.magE -1] = e;
+    if(g.E == NULL){
+        g.E = malloc(sizeof(edge));
+        
+        if(g.E == NULL){
+            return -1;
+        }
+        
+        g.magE++;
+        g.E = e;
+    } else {
+        edge * oldE = malloc(sizeof(edge)*g.magE);
+    
+        for(int i = 0; i < g.magE; i++){
+                oldE[i] = g.E[i];
+        }
+    
+        g.magE++;
+        g.E = realloc(g.E, g.magE);
+        if(g.E == NULL){
+                g.magE--;
+                g.E = oldE;
+                return -1;
+        }
+        g.E[g.magE -1] = e;
+        free(oldE);
+    }
+    
+    return 1;
 }
 
 // Explore a graph.
-void explore(graph & g, vertex & v);
-void BreadthFirstSearch(graph & g, vertex & origin);
-void DepthFirstSearch(graph & g, vertex & origin);
-void Dijkstra(graph & g, vertex & origin);
-void BellmanFord(graph & g, vertex & origin);
+void explore(graph & g, vertex & v){
+    v.visit = TRUE;
+    v.previsit = g.count;
+    g.count++;
+    
+    List * edges = findEdges(g, v);
+    
+    for(List * current = edges; current != NULL; current = current->next){
+        if(((edge *)current->data)->u == v && ((edge *)current->data)->v->visit == FALSE){
+            explore(g, ((edge *)current->data)->*v);
+        }
+    }
+    
+    v.postvisit = g.count;
+    g.count++;
+}
+
+void explore(graph & g, vertex & v, List & l){
+    
+}
+
+void explore(graph & g, vertex & v, int & matrix){
+    
+}
+
+void BreadthFirstSearch(graph & g, vertex & origin){
+    
+}
+
+void DepthFirstSearch(graph & g, vertex & origin){
+    for(int i = 0; i < g.magV; i++){
+        g.V[i].visit = FALSE;
+    }
+    
+    for(int k = 0; k < g.magV; k++){
+        if(g.V[k].visit == FALSE){
+            explore(g, g.V[k]);
+        }
+    }
+/*
+    if(g.magV > g.magE){
+        List * AdjList = createAdjList(graph & g);
+        
+        for(List * current = AdjList; current != NULL; current = current->next){
+            vertex * current_vert = ((vertex *)((List*)current->data)->data);
+            if(current_vert->visit == FALSE){
+                explore(g, current_vert, AdjList); 
+            }
+        }
+    } else {
+        
+    }
+*/
+}
+
+void Dijkstra(graph & g, vertex & origin){
+    
+}
+
+void BellmanFord(graph & g, vertex & origin){
+    
+}
+
 // Partition the graph.
-graph * Partition(graph & g);
+graph * Partition(graph & g){
+    
+}
